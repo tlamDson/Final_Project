b8b6008ae43cc1129143ae112dab3ab4a5ddc1b4
Lam Pham <phamla@dickinson.edu>
2025-11-19 19:33:49 -0500

    feat: Add database connection setup and blog API routes for CRUD operations, commenting, and content generation.

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,4 +1,4 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -10,7 +10,7 @@ import addBlog, {
   getBlogById,
   togglePublish,
   getBlogComments,
-  generateContent
+  generateContent,
 } from "../controllers/blogController.js";
 
 const blogRoutes = express.Router();
@@ -19,16 +19,15 @@ blogRoutes.post("/add", upload.single("image"), auth, addBlog);
 blogRoutes.get("/all", getAllBlogs);
 blogRoutes.post("/delete", auth, deleteBlogById); //only the admin can delete it
 blogRoutes.post("/toggle-publish", auth, togglePublish);
-blogRoutes.post("/add-comment",addComment);
-blogRoutes.post("/comments",getBlogComments);
-blogRoutes.post('/generate',auth,generateContent);
+blogRoutes.post("/add-comment", addComment);
+blogRoutes.post("/comments", getBlogComments);
+blogRoutes.post("/generate", auth, generateContent);
 blogRoutes.get("/:blogId", getBlogById); // Keep dynamic route LAST
 export default blogRoutes;
 
 //next add the  blogRouter into the main file.js
 
 //add the middleware
-
+//gitignore
 //to parse the image use the mullter package
-
 //add another middle ware to protect so that only admin can add this
